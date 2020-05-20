c9c838626554d7d5d8fafb6e7c69fc079140bbf9
Marco Cappa <marcocappa85@gmail.com>
2020-05-20 09:06:13 +0100

    Added endpoints on the console

--- server.ts
+++ server.ts
@@ -7,6 +7,9 @@ const app = new Application();
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-console.log(`Server running on port: ${port}`);
+console.log(`Server running on port: ${port}
+***** Available enpoints:
+http://localhost:${port}/api/v1/products
+`);
 
 await app.listen({ port });
