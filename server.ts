aadc2cb0c938a522572e64a5f47bebba6d3cb542
Marco Cappa <marcocappa85@gmail.com>
2020-05-21 09:36:16 +0100

    fix endpoint

--- server.ts
+++ server.ts
@@ -10,7 +10,7 @@ app.use(router.allowedMethods());
 console.log(`Server running on port: ${port}
 ***** Available enpoints:
 GET     /api/v1/products
-GET     /api/v1/products/1
+GET     /api/v1/products/:id
 POST    /api/v1/products/:id
 PUT     /api/v1/products/:id
 DELETE  /api/v1/products/:id`);
